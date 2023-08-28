0a6ace5cc2b6b5ef1b2fdd52897874ff29cb4617
manoj <manojpoojary2004@gmail.com>
2023-08-28 09:17:59 +0530

    completed project-jsr

--- details.js
+++ details.js
@@ -1,8 +1,18 @@
-let names=localStorage.getItem("name");
+// Accessing id and name of the movie at script.js
+let movie_type_name=localStorage.getItem("name");
 let ids=localStorage.getItem("id")
-let h3=document.getElementById('h3');
-h3.innerHTML=names;
-
+let movie_name_section=document.getElementById('h3');
+movie_name_section.innerHTML=movie_type_name;
+movie_name_section.style.fontSize="25px";
+movie_name_section.style.color="";
+movie_name_section.style.fontWeight="900";
+movie_name_section.style.margin="0%";
+movie_name_section.style.marginBottom="1%";
+movie_name_section.style.padding="2%";
+movie_name_section.style.borderRadius="9px";
+movie_name_section.style.textAlign="center";
+movie_name_section.style.background="linear-gradient(to bottom,#b60f46,#e27c39)";
+// movie_name_section.style.background="linear-gradient(#cc2b5e,#753a88)";
 
 
 let apikey="35e12f6ca3bd8bc9fccc68dcf71236e7";
@@ -16,19 +26,24 @@ let all_movies_gener;
 let all_movies_gener_data=[];
 let all_movies_id=[];
 let all_movies_type=[];
-// let data=[];
 
-
-for (let i = 1; i < 233; i++) {
+// This for loop is used to looping page
+for (let i = 1; i < 10; i++) {
+  // This link 'i' represent the page and 'ids' represent the movie id
    let movies_gener_access_tool="https://api.themoviedb.org/3/discover/movie?api_key="+apikey+"&language=en-US&page="+i+"&vote_average.gte=7.0&with_genres="+ids;
    async function fetch_data() {
     let promise=await fetch(movies_gener_access_tool);
     let convert_json=await promise.json();
+
+    // Accessing results of promise object[convert_json]
     let results= await convert_json.results;
+
+    // all_data variable stored all results of a movie id
     all_data.push(results);
     let data=[];
     data.push(results);
-   //  console.log(data);
+   
+    // 
     function data_access() {
          data[0].forEach((k,l)=> {
             let div=document.getElementById('top');
@@ -42,25 +57,21 @@ for (let i = 1; i < 233; i++) {
             let half_img_path=k.poster_path;
             let full_img_path=img_path+half_img_path;
             imgtag.src=full_img_path;
-            imgtag.width=200;
-            imgtag.height=250;
+            imgtag.width="150";
+            imgtag.height="200";
             imgtag.id=l;
             imgtag.style.borderRadius='10px';
             imgtag.style.padding='4px';
-
-
-
-             imgtag.className=i; 
-            //  console.log(imgtag.className);
-             
+            imgtag.className=i; 
             imgtag.alt="Movies";
             
             document.body.append(div);
             div.appendChild(anchortag); 
-            anchortag.appendChild(imgtag,i);
+            anchortag.appendChild(imgtag);
+
             imgtag.addEventListener('click',function(){
                 let movie_name=k.original_title;
-                  //here access particular results data one by one 
+                //here access particular results data one by one 
                 let movie_overview=k.overview;
                 let movie_releasedate=k.release_date;
                 let movie_average=k.vote_average;
@@ -75,15 +86,13 @@ for (let i = 1; i < 233; i++) {
                 localStorage.setItem("movie_releasedate", movie_releasedate);
                 localStorage.setItem("movie_average", movie_average);
               
-                });
-            
-         }); 
-       
+                }); 
+         });    
     }
     data_access(); 
-   
 };
 fetch_data();
 };
+
 
  
--- moviedetails.js
+++ moviedetails.js
@@ -1,58 +1,217 @@
+// here accessing all localstorage values 
 let m_name = localStorage.getItem("movie_name");
 let m_overview = localStorage.getItem("movie_overview");
 let m_releasedate = localStorage.getItem("movie_releasedate");
 let m_average = localStorage.getItem("movie_average");
 let m_img=localStorage.getItem("movie_img");
+let trailer_id;
+
+// Creating div tag 
+let movie_details_container=document.createElement('div');
+movie_details_container.style.backgroundColor="rgba(0,0,0,0.5)";
+
+
+movie_details_container.style.width="100%";
+movie_details_container.style.height="50%";
+movie_details_container.style.borderRadius="9px";
+movie_details_container.style.paddingBottom="10%";
 
 let div=document.createElement('div');
-div.style.width='500px';
-div.style.padding='5px';
-div.style.borderRadius='30px';
+div.style.width='50%';
+div.style.height='80%';
+div.style.right='55%';
+div.style.paddingTop='1%';
+div.style.paddingLeft='3%';
 
 
-div.style.backgroundColor='black';
 
-
+// Creating h3 tag and h3 tag holding movie name of the movie
 let name_holder=document.createElement('h3');
 name_holder.id="movie_name";
-name_holder.style.color="chocolate";
-name_holder.style.fontSize="25px";
-name_holder.style.fontWeight="100";
-// name_holder.style.textShadow="2px 2px 4px rgba(0,0,0, 0.5)";
+name_holder.style.color="#eebd89";
+
+name_holder.style.fontSize="30px";
+name_holder.style.fontWeight="600";
 name_holder.textContent=m_name;
+
+// Creating paragraph tag and paragraph tag holding overview of the movie
+let overview_heading=document.createElement('div');
+overview_heading.textContent="Overview :";
+overview_heading.style.color="#8bffff";
+overview_heading.style.marginTop="4%";
 
 let overview_holder=document.createElement('p');
 overview_holder.id="movie_overview";
 overview_holder.style.color="white";
+overview_holder.style.marginTop="1%";
 overview_holder.textContent=m_overview;
 
-let date_holder=document.createElement('p');
+// Creating paragraph tag and paragraph tag holding release date of the movie
+
+let date_div=document.createElement('div');
+date_div.style.marginTop="0%";
+let heading_of_date=document.createElement('p');
+heading_of_date.style.marginTop="0%";
+let date_holder=document.createElement('span');
+heading_of_date.textContent="Release Date : ";
+heading_of_date.style.color="#8bffff";
 date_holder.id="movie";
-date_holder.style.color="orange";
+date_holder.style.color="darkgray";
 date_holder.textContent=m_releasedate;
 
-let avg_holder=document.createElement('p');
+// Creating paragraph tag and paragraph tag holding average of the movie
+let average_div=document.createElement('div');
+let star_image=document.createElement('img');
+star_image.src="https://openclipart.org/image/2400px/svg_to_png/253968/GoldStar.png";
+star_image.width='15';
+star_image.height='15';
+
+let avg_holder=document.createElement('span');
 avg_holder.id="movie";
 avg_holder.style.color="yellow";
+avg_holder.style.padding="8px";
 avg_holder.textContent=m_average;
 
+
+// Creating Image tag and image tag holding all data 
 let img_holder=document.createElement('img');
 img_holder.id="movie";
- img_holder.style.padding='12px';
- img_holder.style.borderRadius='30px';
+
+img_holder.style.borderRadius='10%';
 img_holder.src=m_img;
 img_holder.alt="Sorry Image is not found";
-img_holder.style.width='200px';
-img_holder.style.height='250px';
+img_holder.style.maxWidth='20%';
+img_holder.style.maxHeight='40%';
 img_holder.style.position='absolute';
-img_holder.style.top='0px';
-img_holder.style.left='520px';
+img_holder.style.top='4%';
+img_holder.style.left='60%';
 
 
 
+// here append all html tags
 document.body.append(div);
 div.appendChild(name_holder);
-div.appendChild(date_holder);
-div.appendChild(avg_holder);
-div.appendChild(overview_holder);
+
+div.appendChild(date_div);
+date_div.appendChild(heading_of_date);
+heading_of_date.appendChild(date_holder);
+
+div.appendChild(average_div);
+average_div.appendChild(star_image);
+average_div.appendChild(avg_holder);
+
+div.appendChild(overview_heading);
+overview_heading.appendChild(overview_holder);
+
 div.appendChild(img_holder);
+document.body.append(movie_details_container);
+movie_details_container.appendChild(div);
+
+
+// video's section
+let videos_container=document.createElement('div');
+videos_container.style.display="flex";
+videos_container.style.flexDirection="row";
+videos_container.style.justifyContent="center";
+videos_container.style.padding="1%";
+// videos_container.style.marginRight="29%";
+
+
+
+// Movie Trailer accessing section
+let apikey="AIzaSyCa0k_y9XnVB00GwZ8nhBhlUCYIbmlP5rU";
+
+let trailer_apilink="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q="+m_name+"trailer&type=video&key="+apikey;   
+async function trailer_fetching() {
+    let trailerResults=await fetch(trailer_apilink);
+    let jsonconvert=await trailerResults.json();
+    trailer_id=await jsonconvert.items[0].id.videoId;
+    console.log(jsonconvert);
+}
+trailer_fetching();
+
+   let  trailer_container=document.createElement('div');
+   trailer_container.style.ma="10px"
+
+   let trailer_container_heading=document.createElement('h3');
+   trailer_container_heading.textContent="Trailer";
+    trailer_container_heading.style.fontSize="20px";
+    trailer_container_heading.style.fontWeight="900";
+
+    let movie_trailer_holder=document.createElement('a');
+    movie_trailer_holder.href="https://www.youtube.com/search?q="+m_name+" trailer";
+    movie_trailer_holder.style.color="red";
+
+
+
+    let trailer_logo=document.createElement('img');
+    trailer_logo.src="https://media.istockphoto.com/id/937037522/photo/video-play-button-3d.jpg?s=612x612&w=0&k=20&c=v6ELtB2WitrxrtNwcey8K0FEJqvauc1JrIaE5j3971Y=";
+    trailer_logo.style.width='50px';
+    trailer_logo.style.height='50px';
+    trailer_logo.alt="Trailer";
+    trailer_logo.style.borderRadius="12%";
+    trailer_logo.style.padding="1%";
+    trailer_logo.style.marginLeft="2px"
+    
+    document.body.append(videos_container);
+    videos_container.appendChild(trailer_container);
+    trailer_container.appendChild(trailer_container_heading);
+    trailer_container.appendChild(movie_trailer_holder);
+    movie_trailer_holder.appendChild(trailer_logo);
+    
+
+
+let music_container=document.createElement('div');
+music_container.style.paddingLeft="5%"
+let music_container_heading=document.createElement('h3');
+music_container_heading.textContent="Music";
+music_container_heading.style.fontSize="20px";
+music_container_heading.style.fontWeight="900";
+
+
+let audio=document.createElement('a');
+audio.href="https://music.youtube.com/search?q="+m_name+" songs";
+
+let audio_logo=document.createElement('img');
+audio_logo.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZa_inleDCVx40Ex6LobToAZY506r4_a-pTbLAFyGJboP0xUxPfcQLHwCAmlC2QLQp9ck&usqp=CAU";
+audio_logo.alt="This image is not loading...";
+audio_logo.style.width="50px";
+audio_logo.style.height="50px";
+audio_logo.style.borderRadius="12%";
+audio_logo.style.padding="1%";
+
+
+videos_container.appendChild(music_container);
+music_container.appendChild(music_container_heading);
+music_container.appendChild(audio);
+audio.appendChild(audio_logo);
+
+let movie_review_container=document.createElement('div');
+movie_review_container.style.paddingLeft="5%";
+
+
+
+let movie_review_heading=document.createElement('h3');
+movie_review_heading.textContent="Reviews";
+movie_review_heading.style.fontSize="20px";
+movie_review_heading.style.fontWeight="900";
+
+
+let movie_review_linking=document.createElement('a');
+movie_review_linking.href="https://www.youtube.com/search?q="+m_name+" reviews";
+console.log(movie_review_linking);
+let movie_review_logo=document.createElement('img');
+movie_review_logo.src="https://thumbs.dreamstime.com/b/movie-reviews-icon-elegant-cyan-blue-diamond-button-movie-reviews-icon-isolated-elegant-cyan-blue-diamond-button-abstract-104730279.jpg";
+movie_review_logo.alt="This image is not loading...";
+movie_review_logo.style.width="50px";
+movie_review_logo.style.height="50px";
+movie_review_logo.style.marginLeft="10%";
+movie_review_logo.style.borderRadius="10%";
+
+
+videos_container.appendChild(movie_review_container);
+movie_review_container.appendChild(movie_review_heading);
+movie_review_container.appendChild(movie_review_linking);
+movie_review_linking.appendChild(movie_review_logo);
+
+
