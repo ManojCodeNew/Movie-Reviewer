c52261a0c7c173af09787da57910289eb00eb368
manoj <manojpoojary2004@gmail.com>
2024-02-21 23:58:20 +0530

    search feature completed

--- search.js
+++ search.js
@@ -2,6 +2,7 @@ let convert_into_json;
 let final_data_of_json = [];
 let search_box_div = document.getElementById('search_box_div');
 let search_btn = document.getElementById('search_btn');
+let movie_poster_container = document.getElementById('movie_poster_container');
 
 let Search_box = document.createElement('input');
 Search_box.type = 'search';
@@ -19,47 +20,88 @@ let finded_movies = [];
 search_btn.addEventListener('click', function () {
     movie_name = Search_box.value;
     fetch_movie(movie_name);
+    clearMoviePosters();
+
 })
 
 
 async function fetch_movie(moviename) {
-    if (moviename=='') {
+    if (moviename == '') {
         alert('Enter movie name')
     }
     let movie_searching_link = `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${moviename}`;
-    let fetch_movie_section=await fetch(movie_searching_link);
-    let convert_into_json= await fetch_movie_section.json();
-    let movie_details=convert_into_json.results;
+    let fetch_movie_section = await fetch(movie_searching_link);
+    let convert_into_json = await fetch_movie_section.json();
+    let movie_details = convert_into_json.results;
     display_movie(movie_details);
-    
+
 }
-function display_movie(movie_details){
+function display_movie(movie_details) {
     if (!movie_details) {
         alert("Error")
     }
-    let Movie_posters_container=document.createElement('div');
-    clearMoviePosters(Movie_posters_container);
     movie_details.forEach(items => {
+        let first_half_poster_path = "https://image.tmdb.org/t/p/w500";
+        let poster_linking = document.createElement('a');
+        poster_linking.href = 'moviedetails.html';
+        let poster = document.createElement('img');
 
-        console.log("display_movie",items);
-        let first_half_poster_path = "https://image.tmdb.org/t/p/w500";    
-        Movie_posters_container.id="poster_container";
+        if (items.poster_path) {
+            // poster.src = first_half_poster_path + '/bogkgrgdhrbyjslpxaxhxvstddv.jpg/'
 
 
-        let poster=document.createElement('img');
-        poster.src=first_half_poster_path + items.poster_path;
-        poster.alt=items.title;
-        poster.width='200px';
-        poster.height='250px';
-        document.body.append(Movie_posters_container)
-        Movie_posters_container.appendChild(poster)
+            poster.src = first_half_poster_path + items.poster_path;
+            poster.alt = items.title;
+            poster.className = "poster";
+            poster.style.width = "150px";
+            poster.style.height = "200px";
+            poster.style.marginTop = "3%";
+
+            poster.style.borderRadius = '15px';
+            poster.style.padding = '3px';
+            poster.style.cursor = 'pointer';
+            movie_poster_container.appendChild(poster_linking)
+            poster_linking.appendChild(poster);
+        }
+        poster.addEventListener('mouseover',function() {
+            poster.style.width='170px';
+            poster.style.height='205px';
+            poster.style.opacity='0.7';
+
+
+        })
+        poster.addEventListener('mouseout',function() {
+            poster.style.width='150px';
+            poster.style.height='200px';
+            // poster.style.opacity='';
+
+
+        })
+        poster.addEventListener('click', function () {
+            
+            let movie_img = first_half_poster_path + items.poster_path;
+            let movie_name = items.title;
+            let movie_average = items.vote_average;
+            let movie_overview = items.overview;
+            let movie_releasedate = items.release_date;
+
+            localStorage.setItem("movie_img", movie_img);
+            localStorage.setItem("movie_name", movie_name);
+            localStorage.setItem("movie_overview", movie_overview);
+            localStorage.setItem("movie_releasedate", movie_releasedate);
+            localStorage.setItem("movie_average", movie_average);
+
+        })
+
 
     });
 
 }
-function clearMoviePosters(poster_container) {
-    // let poster_container=document.getElementById('poster_container');
-    poster_container.innerHTML=' Not Found';
+function clearMoviePosters() {
+    // let poster_container=document.getElementById('#poster_container');
+    // poster_container.innerHTML=' Not Found';
+    movie_poster_container.innerHTML = ''
+
 }
 
 
