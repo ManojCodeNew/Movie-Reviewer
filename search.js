525989b16fc0d3e0fd67aee4433205d13fb65042
manoj <manojpoojary2004@gmail.com>
2024-02-21 22:56:12 +0530

    search option

--- search.js
+++ search.js
@@ -9,107 +9,132 @@ Search_box.value = '';
 Search_box.placeholder = "Enter the Movie name..";
 Search_box.style.padding = "15px";
 Search_box.style.borderRadius = "15px";
-Search_box.onchange = onChangehandle;
+// Search_box.onchange = onChangehandle;
 search_box_div.appendChild(Search_box);
 search_box_div.appendChild(search_btn);
-// let autoSuggestionBox = document.createElement('div');
-
-
-// Search_box.addEventListener('click', () => {
-//     autoSuggestionBox.className = 'autoSuggest';
-//     let ispresent = document.querySelector('.autoName')
-//     if (!ispresent) {
-//         for (let i = 0; i < 5; i++) {
-//             let autoSuggestionName = document.createElement('div');
-//             autoSuggestionName.className = 'autoName';
-//             autoSuggestionName.innerHTML = 'Jawan';
-//             autoSuggestionBox.appendChild(autoSuggestionName)
-//         }
-//         search_box_div.appendChild(autoSuggestionBox)
-//     }
-
-
-// })
-
-const movie_search_name = []
-let All_movie = []
-
-// let search_data='';
 
 let apikey = "35e12f6ca3bd8bc9fccc68dcf71236e7";
-// console.log(Search_box.value);
+let finded_movies = [];
 
-for (i = 0; i < 200; i++) {
-    let movies_access_tool = "https://api.themoviedb.org/3/discover/movie?api_key=" + apikey + "&language=en-US&page=" + i + "&vote_average.gte=7.0";
-    async function Fetch_data() {
-        let api_provide_data = await fetch(movies_access_tool)
-        convert_into_json = await api_provide_data.json();
-        final_data_of_json.push(convert_into_json)
-        final_data_of_json.filter((response) => {
-            // console.log(response.results);
-            All_movie.push(response.results)
-        })
+search_btn.addEventListener('click', function () {
+    movie_name = Search_box.value;
+    fetch_movie(movie_name);
+})
 
+
+async function fetch_movie(moviename) {
+    if (moviename=='') {
+        alert('Enter movie name')
     }
-
-    Fetch_data();
+    let movie_searching_link = `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${moviename}`;
+    let fetch_movie_section=await fetch(movie_searching_link);
+    let convert_into_json= await fetch_movie_section.json();
+    let movie_details=convert_into_json.results;
+    display_movie(movie_details);
+    
 }
-let img_linking = document.createElement('a');
-img_linking.href = "moviedetails.html";
-let poster_container = document.createElement('img');
+function display_movie(movie_details){
+    if (!movie_details) {
+        alert("Error")
+    }
+    let Movie_posters_container=document.createElement('div');
+    clearMoviePosters(Movie_posters_container);
+    movie_details.forEach(items => {
 
-function onChangehandle(e) {
-    let search_data = e.target.value;
-    search_btn.addEventListener('click', function () {
-        // search_box_div.removeChild(autoSuggestionBox)
-        if (All_movie) {
-            All_movie.some((element, index) => {
-                return element.some((movie, i) => {
-                    console.log(movie.title);
-
-                    if (movie.title == search_data) {
-                        console.log("Found");
-
-                        let movie_poster_container = document.createElement('div');
-
-                        movie_poster_container.id = "movie_poster";
-                        poster_container.id = "poster";
-                        let first_half_poster_path = "https://image.tmdb.org/t/p/w500";
-                        poster_container.src = first_half_poster_path + movie.poster_path;
-                        poster_container.alt = movie.title;
-                        poster_container.style.borderRadius = '10px';
-                        // poster_container.style.padd = '20px';
-
-                        poster_container.style.width = "200px";
-                        poster_container.style.height = "250px";
-                        document.body.append(movie_poster_container);
-                        movie_poster_container.appendChild(img_linking);
-                        img_linking.appendChild(poster_container);
+        console.log("display_movie",items);
+        let first_half_poster_path = "https://image.tmdb.org/t/p/w500";    
+        Movie_posters_container.id="poster_container";
 
 
+        let poster=document.createElement('img');
+        poster.src=first_half_poster_path + items.poster_path;
+        poster.alt=items.title;
+        poster.width='200px';
+        poster.height='250px';
+        document.body.append(Movie_posters_container)
+        Movie_posters_container.appendChild(poster)
 
-                        let movie_img = first_half_poster_path + movie.poster_path;
-                        let movie_name = movie.title;
-                        let movie_average = movie.vote_average;
-                        let movie_overview = movie.overview;
-                        let movie_releasedate = movie.release_date;
-
-                        localStorage.setItem("movie_img", movie_img);
-                        localStorage.setItem("movie_name", movie_name);
-                        localStorage.setItem("movie_overview", movie_overview);
-                        localStorage.setItem("movie_releasedate", movie_releasedate);
-                        localStorage.setItem("movie_average", movie_average);
-
-                        return true;
-                    }
-
-                })
-            })
-
-        }
-    })
+    });
 
 }
+function clearMoviePosters(poster_container) {
+    // let poster_container=document.getElementById('poster_container');
+    poster_container.innerHTML=' Not Found';
+}
 
-// console.log(All_movie.);
+
+// async function fetch_movie() {
+//     let fetch_movie = await fetch(movie_searching_link);
+//     let convert_into_json = await fetch_movie.json();
+//     let response = [convert_into_json]
+
+//     let movie_details = []
+//     search_btn.addEventListener('click', function () {
+//         console.log("length", e.target.value.length);
+//         response.filter((movie) => {
+//             movie_details.push(movie.results)
+//         })
+//         movie_details.forEach((data) => {
+//             let depth_movie = [data]
+//             console.log(depth_movie);
+
+//             depth_movie[0].forEach((items, i) => {
+//                 let img_linking = document.createElement('a');
+//                 img_linking.href = "moviedetails.html";
+//                 let movie_poster_container = document.createElement('div');
+//                 movie_poster_container.id = i;
+//                 let poster_container = document.createElement('img');
+
+//                 if (items) {
+
+//                     console.log("i", items);
+//                     movie_poster_container.id = "movie_poster";
+//                     poster_container.id = "poster";
+//                     let first_half_poster_path = "https://image.tmdb.org/t/p/w500";
+//                     poster_container.src = first_half_poster_path + items.poster_path;
+//                     poster_container.alt = items.title;
+//                     poster_container.style.borderRadius = '10px';
+
+//                     poster_container.style.width = "200px";
+//                     poster_container.style.height = "250px";
+//                     document.body.append(movie_poster_container);
+//                     movie_poster_container.appendChild(img_linking);
+//                     img_linking.appendChild(poster_container);
+
+
+
+//                     let movie_img = first_half_poster_path + items.poster_path;
+//                     let movie_name = items.title;
+//                     let movie_average = items.vote_average;
+//                     let movie_overview = items.overview;
+//                     let movie_releasedate = items.release_date;
+
+//                     localStorage.setItem("movie_img", movie_img);
+//                     localStorage.setItem("movie_name", movie_name);
+//                     localStorage.setItem("movie_overview", movie_overview);
+//                     localStorage.setItem("movie_releasedate", movie_releasedate);
+//                     localStorage.setItem("movie_average", movie_average);
+//                 }
+//                 else {
+//                     alert("Sorry, Not Found!!!")
+//                 }
+
+//             })
+//             movie_details = []
+//             depth_movie = []
+//             response = []
+//         })
+//     })
+
+// }
+
+
+// console.log("Movie",finded_movies.results);
+
+
+
+
+// Search_box.addEventListener('input',onChangehandle)
+// const movie_search_name = []
+// let All_movie = []
 
