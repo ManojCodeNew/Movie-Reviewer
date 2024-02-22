a3f9b42cfe309dbc00fe150e09947e303a33d11a
manoj <manojpoojary2004@gmail.com>
2024-02-22 09:35:24 +0530

    Movie Reviewer Final work

--- search.js
+++ search.js
@@ -10,7 +10,6 @@ Search_box.value = '';
 Search_box.placeholder = "Enter the Movie name..";
 Search_box.style.padding = "15px";
 Search_box.style.borderRadius = "15px";
-// Search_box.onchange = onChangehandle;
 search_box_div.appendChild(Search_box);
 search_box_div.appendChild(search_btn);
 
@@ -73,7 +72,6 @@ function display_movie(movie_details) {
         poster.addEventListener('mouseout',function() {
             poster.style.width='150px';
             poster.style.height='200px';
-            // poster.style.opacity='';
 
 
         })
@@ -98,85 +96,6 @@ function display_movie(movie_details) {
 
 }
 function clearMoviePosters() {
-    // let poster_container=document.getElementById('#poster_container');
-    // poster_container.innerHTML=' Not Found';
     movie_poster_container.innerHTML = ''
 
 }
-
-
-// async function fetch_movie() {
-//     let fetch_movie = await fetch(movie_searching_link);
-//     let convert_into_json = await fetch_movie.json();
-//     let response = [convert_into_json]
-
-//     let movie_details = []
-//     search_btn.addEventListener('click', function () {
-//         console.log("length", e.target.value.length);
-//         response.filter((movie) => {
-//             movie_details.push(movie.results)
-//         })
-//         movie_details.forEach((data) => {
-//             let depth_movie = [data]
-//             console.log(depth_movie);
-
-//             depth_movie[0].forEach((items, i) => {
-//                 let img_linking = document.createElement('a');
-//                 img_linking.href = "moviedetails.html";
-//                 let movie_poster_container = document.createElement('div');
-//                 movie_poster_container.id = i;
-//                 let poster_container = document.createElement('img');
-
-//                 if (items) {
-
-//                     console.log("i", items);
-//                     movie_poster_container.id = "movie_poster";
-//                     poster_container.id = "poster";
-//                     let first_half_poster_path = "https://image.tmdb.org/t/p/w500";
-//                     poster_container.src = first_half_poster_path + items.poster_path;
-//                     poster_container.alt = items.title;
-//                     poster_container.style.borderRadius = '10px';
-
-//                     poster_container.style.width = "200px";
-//                     poster_container.style.height = "250px";
-//                     document.body.append(movie_poster_container);
-//                     movie_poster_container.appendChild(img_linking);
-//                     img_linking.appendChild(poster_container);
-
-
-
-//                     let movie_img = first_half_poster_path + items.poster_path;
-//                     let movie_name = items.title;
-//                     let movie_average = items.vote_average;
-//                     let movie_overview = items.overview;
-//                     let movie_releasedate = items.release_date;
-
-//                     localStorage.setItem("movie_img", movie_img);
-//                     localStorage.setItem("movie_name", movie_name);
-//                     localStorage.setItem("movie_overview", movie_overview);
-//                     localStorage.setItem("movie_releasedate", movie_releasedate);
-//                     localStorage.setItem("movie_average", movie_average);
-//                 }
-//                 else {
-//                     alert("Sorry, Not Found!!!")
-//                 }
-
-//             })
-//             movie_details = []
-//             depth_movie = []
-//             response = []
-//         })
-//     })
-
-// }
-
-
-// console.log("Movie",finded_movies.results);
-
-
-
-
-// Search_box.addEventListener('input',onChangehandle)
-// const movie_search_name = []
-// let All_movie = []
-
